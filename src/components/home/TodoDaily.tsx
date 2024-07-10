660f2c4099b7e1f512530f06f476a6146ddc1951
Pedroo07 <pedrohenriquemoalive@gmail.com>
2024-07-10 11:00:25 -0300

    creating function to delete item from list

--- src/components/home/TodoDaily.tsx
+++ src/components/home/TodoDaily.tsx
@@ -1,10 +1,18 @@
 import { EllipsisVertical } from "lucide-react";
 import React, { useState } from "react";
+import {
+    DropdownMenu, DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Trash2 } from "lucide-react";
 
-interface CustomCheckboxProps {
+type CustomCheckboxProps = {
     isChecked: boolean;
     handleCheckboxChange: () => void;
+    onDelete? : () => void
 }
+
 
 const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ isChecked, handleCheckboxChange }) => (
     <>
@@ -41,11 +49,16 @@ const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ isChecked, handleCheckb
 
 export const TodoDaily = () => {
     const [isChecked, setIsChecked] = useState(false);
+    const [items, setItems] = useState<string[]>(['1'])
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
     };
-
+    const handleDeleteItem = (index: number) => {
+        const updateItems = [...items]
+        updateItems.splice(index, 1)
+        setItems(updateItems)
+    }
     return (
         <div className="max-w-md mx-auto my-8">
             <div className="flex justify-between mb-4">
@@ -53,13 +66,15 @@ export const TodoDaily = () => {
                 <h6 className="text-color1 font-semibold cursor-pointer">See all</h6>
             </div>
             <ul className="flex flex-col gap-2 items-center text-start">
-                <TodoDailyItem isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} />
+                {items.map((item, index) => (
+                <TodoDailyItem key={index} isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} onDelete={() => handleDeleteItem(index)} />
+                ))}
             </ul>
         </div>
     );
 };
 
-const TodoDailyItem: React.FC<CustomCheckboxProps> = ({isChecked, handleCheckboxChange}) => {
+const TodoDailyItem: React.FC<CustomCheckboxProps> = ({ isChecked, handleCheckboxChange,onDelete }) => {
     return (
         <li
             className={`w-full py-3 rounded px-1 flex justify-between items-center transition-colors ${isChecked ? 'bg-green-100' : 'bg-gray-100'}`}
@@ -67,7 +82,12 @@ const TodoDailyItem: React.FC<CustomCheckboxProps> = ({isChecked, handleCheckbox
             <span className={`font-semibold ${isChecked ? 'text-green-500' : ''}`}>Medisasa</span>
             <span className="flex items-center">
                 <CustomCheckbox isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} />
-                <EllipsisVertical size={18} className="text-slate-600 ml-2 cursor-pointer" />
+                <DropdownMenu>
+                    <DropdownMenuTrigger><EllipsisVertical size={18} className="text-slate-600 ml-2 cursor-pointer" /></DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem className="justify-center" onClick={onDelete}><Trash2 size={18}/></DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </span>
         </li>
     )
