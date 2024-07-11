b66c63eacd5e48a06879725a6a03385dfd77fa16
Pedroo07 <pedrohenriquemoalive@gmail.com>
2024-07-11 09:38:20 -0300

    Creating function to add new Todo-Item

--- src/components/home/TodoDaily.tsx
+++ src/components/home/TodoDaily.tsx
@@ -1,30 +1,51 @@
-import { EllipsisVertical } from "lucide-react";
-import React, { useState } from "react";
+import { EllipsisVertical, PlusCircleIcon } from "lucide-react";
+import React, { ChangeEvent, useState } from "react";
 import {
     DropdownMenu, DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "../ui/dialog"
 import { Trash2 } from "lucide-react";
 
 type CustomCheckboxProps = {
+    id: number
     isChecked: boolean;
     handleCheckboxChange: () => void;
-    onDelete? : () => void
+}
+
+type TodoItem = {
+    id: number
+    text: string
+    isChecked: boolean
+}
+
+type TodoDailyItemProps = {
+    item: TodoItem;
+    handleCheckboxChange: (id: number) => void;
+    onDelete: (id: number) => void;
 }
 
 
-const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ isChecked, handleCheckboxChange }) => (
+
+const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ id, isChecked, handleCheckboxChange }) => (
     <>
         <input
             type="checkbox"
-            id="custom-checkbox"
-            className="hidden"
+            id={`custom-checkbox-${id}`}
+            className="sr-only"
             checked={isChecked}
             onChange={handleCheckboxChange}
         />
         <label
-            htmlFor="custom-checkbox"
+            htmlFor={`custom-checkbox-${id}`}
             className={`w-8 h-8 flex items-center justify-center border-2 rounded-md cursor-pointer transition-colors ${isChecked ? 'bg-bg-color2 border-none' : 'bg-white border-gray-400'}`}
         >
             {isChecked && (
@@ -46,49 +67,93 @@ const CustomCheckbox: React.FC<CustomCheckboxProps> = ({ isChecked, handleCheckb
         </label>
     </>
 );
-
-export const TodoDaily = () => {
-    const [isChecked, setIsChecked] = useState(false);
-    const [items, setItems] = useState<string[]>(['1'])
-
-    const handleCheckboxChange = () => {
-        setIsChecked(!isChecked);
-    };
-    const handleDeleteItem = (index: number) => {
-        const updateItems = [...items]
-        updateItems.splice(index, 1)
-        setItems(updateItems)
-    }
-    return (
-        <div className="max-w-md mx-auto my-8">
-            <div className="flex justify-between mb-4">
-                <h2 className="text-xl font-semibold">Today Habit</h2>
-                <h6 className="text-color1 font-semibold cursor-pointer">See all</h6>
-            </div>
-            <ul className="flex flex-col gap-2 items-center text-start">
-                {items.map((item, index) => (
-                <TodoDailyItem key={index} isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} onDelete={() => handleDeleteItem(index)} />
-                ))}
-            </ul>
-        </div>
-    );
-};
-
-const TodoDailyItem: React.FC<CustomCheckboxProps> = ({ isChecked, handleCheckboxChange,onDelete }) => {
+const TodoDailyItem: React.FC<TodoDailyItemProps> = ({ item, handleCheckboxChange, onDelete }) => {
     return (
         <li
-            className={`w-full py-3 rounded px-1 flex justify-between items-center transition-colors ${isChecked ? 'bg-green-100' : 'bg-gray-100'}`}
+            className={`w-full py-3 rounded px-1 flex justify-between items-center transition-colors ${item.isChecked ? 'bg-green-100' : 'bg-gray-100'}`}
         >
-            <span className={`font-semibold ${isChecked ? 'text-green-500' : ''}`}>Medisasa</span>
+            <span className={`font-semibold ${item.isChecked ? 'text-green-500' : ''}`}>{item.text}</span>
             <span className="flex items-center">
-                <CustomCheckbox isChecked={isChecked} handleCheckboxChange={handleCheckboxChange} />
+                <CustomCheckbox id={item.id} isChecked={item.isChecked} handleCheckboxChange={() => handleCheckboxChange(item.id)} />
                 <DropdownMenu>
                     <DropdownMenuTrigger><EllipsisVertical size={18} className="text-slate-600 ml-2 cursor-pointer" /></DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuItem className="justify-center" onClick={onDelete}><Trash2 size={18}/></DropdownMenuItem>
+                        <DropdownMenuItem className="justify-center" onClick={() => onDelete(item.id)}><Trash2 size={18} /></DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </span>
         </li>
     )
 }
+export const TodoDaily = () => {
+    const [title, setTitle] = useState<string>("")
+    const [items, setItems] = useState<TodoItem[]>([
+        { id: 1, text: "Item 1", isChecked: false },
+        { id: 2, text: "Item 2", isChecked: false },
+        { id: 3, text: "Item 3", isChecked: false },])
+
+    const handleCheckboxChange = (id: number) => {
+        setItems(items.map(item => item.id === id ? { ...item, isChecked: !item.isChecked } : item))
+    }
+    const handleDeleteItem = (id: number) => {
+        setItems(items.filter(item => item.id !== id));
+    }
+    const handleTitleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value
+        setTitle(newValue)
+    }
+    const handleAddTodoItem = () => {
+        const newItem: TodoItem = {
+            id: items.length + 1,
+            text: title,
+            isChecked: false
+        }
+        setItems([...items, newItem])
+        setTitle('')
+    }
+    return (
+        <div className="max-w-md mx-auto my-8">
+            <div className="flex justify-between mb-4">
+                <h2 className="text-xl font-semibold">Today Habit</h2>
+                <Dialog>
+                    <DialogTrigger>
+                        <button className="bg-bg-color2 rounded-full h-7 w-7 items-center justify-center flex text-white"><PlusCircleIcon className="w-full h-full" />
+                        </button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Add a new To-do</DialogTitle>
+                        </DialogHeader>
+                        <div className="flex items-center gap-4">
+                            <label htmlFor="title" className="text-right font-medium">Title:</label>
+                            <input
+                                id="title"
+                                type="text"
+                                placeholder="Add new todo item..."
+                                value={title}
+                                autoFocus={true}
+                                onChange={handleTitleChange}
+                                className="border-gray-400 border-solid border-2 rounded-sm p-0.5" />
+                        </div>
+                        <DialogFooter>
+                            <button
+                                onClick={handleAddTodoItem}
+                                type="button"
+                                className="bg-bg-color2 items-center px-3 rounded-sm flex gap-1 text-white"
+                                disabled={!title}
+                            >
+                                <PlusCircleIcon size={16} />
+                                <span>Add</span>
+                            </button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            </div>
+            <ul className="flex flex-col gap-2 items-center text-start">
+                {items.map(item => (
+                    <TodoDailyItem key={item.id} item={item} handleCheckboxChange={handleCheckboxChange} onDelete={handleDeleteItem} />
+                ))}
+            </ul>
+        </div>
+    );
+};
