4c7467dbd4056f1b1d15a7f0ee512e7900185694
Pedroo07 <pedrohenriquemoalive@gmail.com>
2024-07-22 22:11:27 -0300

    Add Real-Time Progress Updates

--- src/components/home/GoalItem.tsx
+++ src/components/home/GoalItem.tsx
@@ -1,4 +1,4 @@
-import { formatDistanceToNow } from "date-fns"
+import { differenceInMinutes, formatDistanceToNow } from "date-fns"
 import { ptBR } from "date-fns/locale"
 import { useEffect, useState } from "react"
 
@@ -6,12 +6,30 @@ import { useEffect, useState } from "react"
 type GoalItemPops = {
     title: string
     date: Date
+    goalTime: number
 }
 
-export const GoalItem: React.FC<GoalItemPops> = ({title, date}) => {
+export const GoalItem: React.FC<GoalItemPops> = ({ title, date, goalTime }) => {
     const [timeAgo, setTimeAgo] = useState<string>('')
+    const [progress, setProgess] = useState<number>(0)
+
+    const caulculateProgress = () => {
+        const now = new Date()
+        const minutesElapesd = differenceInMinutes(now, date)
+        const goalTimeInMinutes = goalTime * 24 * 60
+        const percentage = (minutesElapesd / goalTimeInMinutes) * 100
+        setProgess(percentage)
+    }
+    useEffect(() => {
+        caulculateProgress()
+
+        const intervalId = setInterval(caulculateProgress, 60000)
+
+        return() =>  clearInterval(intervalId)
+    }, [date, goalTime])
 
     useEffect(() => {
+
         const caulculateTimeAgo = () => {
             const timePassed = formatDistanceToNow(date, {
                 addSuffix: true,
@@ -37,11 +55,11 @@ export const GoalItem: React.FC<GoalItemPops> = ({title, date}) => {
                 </div>
                 <div>
                     <div className="flex justify-between text-xs font-bold">
-                        <span>Current goal: 10 days</span>
-                        <span>50%</span>
+                        <span>Current goal: {goalTime} days</span>
+                        <span>{progress.toFixed(1)}%</span>
                     </div>
                     <div className="w-full bg-gray-300 h-1 rounded">
-                        <div className="w-2/4 h-1 bg-bg-color1 rounded"></div>
+                        <div className='h-1 bg-bg-color1 rounded' style={{width: `${progress}%`}}></div>
                     </div>
                 </div>
             </div>
--- src/components/home/Goals.tsx
+++ src/components/home/Goals.tsx
@@ -9,9 +9,9 @@ export const Goals = () => {
                 <span className="text-color1 font-semibold cursor-pointer" >See all</span>
             </div>
             <div className="flex flex-col gap-2">
-                <GoalItem title="Alcool" date={createdAt}/>
-                <GoalItem title="Porn"  date={createdAt}/>
-                <GoalItem title="Procastination" date={createdAt} />
+                <GoalItem title="Alcool" date={createdAt} goalTime={10} />
+                <GoalItem title="Porn" date={createdAt} goalTime={10} />
+                <GoalItem title="Procastination" date={createdAt} goalTime={10} />
             </div>
         </div>
     )
